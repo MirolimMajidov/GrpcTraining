e793d893fd7d3d174d67015b083c9eb267754f81
Mirolim Majidov <mirolimmajidov@outlook.com>
2026-04-08 10:05:44 +0500

    Created empty projects

---
+++ GrpcServiceApi/Services/IUserManagerService.cs
@@ -0,0 +1,11 @@
+﻿using GrpcServiceApi.Entities;
+
+namespace GrpcServiceApi.Services;
+
+public interface IUserManagerService
+{
+    public User[] GetAll(Guid userId);
+    public User GetById(Guid userId);
+    public bool Update(Guid userId, User newUser);
+    public bool DeleteById(Guid userId);
+}
---
+++ GrpcServiceApi/Services/UserManagerService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+using GrpcServiceApi.Entities;
+
+namespace GrpcServiceApi.Services;
+
+public class UserManagerService : IUserManagerService
+{
+    private readonly ConcurrentDictionary<Guid, User> _users = new();
+
+    public User[] GetAll(Guid userId)
+    {
+        return _users.Values.ToArray();
+    }
+
+    public User GetById(Guid userId)
+    {
+        _users.TryGetValue(userId, out var user);
+        return user;
+    }
+
+    public bool Update(Guid userId, User newUser)
+    {
+        if (!_users.ContainsKey(userId))
+            return false;
+
+        _users[userId] = newUser;
+        return true;
+    }
+
+    public bool DeleteById(Guid userId)
+    {
+        return _users.TryRemove(userId, out _);
+    }
+}
